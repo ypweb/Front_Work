74c5490adb26a734a2fe12c1647a304623f430c6
yipin8715@sina.com <yipin8715@sina.com>
2018-04-16 18:03:25 +0800

    mall_admin_vue

---
+++ mall_admin_vue/mall_admin_vue配置备份/src/components/layout/login_panel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>login</div>
+</template>
+
+<script>
+export default {
+  name: 'ch_login_panel',
+  data () {
+    return {}
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- mall_admin_vue/src/main.js
+++ mall_admin_vue/src/main.js
@@ -1,3 +1,4 @@
+/*导入相关组件*/
 import Vue from 'vue';
 import iView from 'iview';
 import VueRouter from 'vue-router';
@@ -5,35 +6,40 @@ import Routers from './router';
 import Vuex from 'vuex';
 import Util from './libs/util';
 import App from './app.vue';
+
+/*导入iview样式*/
 import 'iview/dist/styles/iview.css';
 
-
+/*使用路由*/
 Vue.use(VueRouter);
+/*使用vuex*/
 Vue.use(Vuex);
-
+/*使用iview*/
 Vue.use(iView);
 
 
 
-// 路由配置
+/*路由配置*/
 const RouterConfig = {
     mode: 'history',
     routes: Routers
 };
 const router = new VueRouter(RouterConfig);
 
+/*路由加载前*/
 router.beforeEach((to, from, next) => {
     iView.LoadingBar.start();
     Util.title(to.meta.title);
     next();
 });
 
+/*路由加载后*/
 router.afterEach(() => {
     iView.LoadingBar.finish();
     window.scrollTo(0, 0);
 });
 
-
+/*数据管理*/
 const store = new Vuex.Store({
     state: {
 
