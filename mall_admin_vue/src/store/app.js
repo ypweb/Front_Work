598bac2fd3f723526b6750f05f8dc230b79f3981
yipin8715@sina.com <yipin8715@sina.com>
2018-04-24 20:09:45 +0800

    mall_admin_vue

--- mall_admin_vue/src/store/app.js
+++ mall_admin_vue/src/store/app.js
@@ -1,4 +1,5 @@
 import Tool from './../libs/tool';
+import moment from './../../node_modules/moment';
 import axios from 'axios';
 
 export default {
@@ -7,7 +8,8 @@ export default {
         debug: true, /*测试模式*/
         issupport: (Tool.supportImage && Tool.supportStorage && Tool.supportBox()), /*是否兼容*/
         islogin: false, /*是否登录*/
-        isfzf: false/*是否路径正确即是否404错误*/
+        isfzf: false/*是否路径正确即是否404错误*/,
+        cache:Tool.getParams(Tool.getSystemUniqueKey())/*缓存信息*/
     },
     getters: {},
     mutations: {
@@ -15,10 +17,11 @@ export default {
         changeLogin(state, flag) {
             /*登录成功时进行持久保存*/
             if (flag) {
-                let systemkey = Tool.getSystemUniqueKey(),
-                    cache = Tool.getParams(systemkey);
                 /*如果不存在缓存则创建缓存*/
-                if (cache === null) {
+                let logininfo=false;
+                if (state.cache === null) {
+
+                }else{
 
                 }
             }else{
@@ -38,6 +41,43 @@ export default {
         },
         changeFZF(state, flag) {
             state.isfzf = flag;
+        },
+        /*校验缓存是否有效*/
+        validLogin(state){
+            if(state.cache!==null){
+                /*存在缓存*/
+                let login_dt=state.cache.datetime;
+                if(!login_dt){
+                    /*非法时间*/
+                    return false;
+                }
+                login_dt = login_dt.replace(/\s*/g, '').split('|');
+
+                let login_rq = login_dt[0],
+                    login_sj = login_dt[1],
+                    now = moment().format('YYYY-MM-DD|HH:mm:ss').split('|'),
+                    now_rq = now[0],
+                    now_sj = now[1];
+
+                /*判断日期*/
+                if (login_rq !== now_rq) {
+                    //同一天有效
+                    return false;
+                } else if (login_rq === now_rq) {
+                    login_sj = login_sj.split(':');
+                    now_sj = now_sj.split(':');
+                    let login_hh = parseInt(login_sj[0], 10),
+                        now_hh = parseInt(now_sj[0], 10);
+
+                    if (now_hh - login_hh > 2) {
+                        /*超过2个小时退出*/
+                        return false;
+                    }
+                }
+            }else{
+                /*不存在缓存*/
+                return false;
+            }
         }
     },
     actions: {
